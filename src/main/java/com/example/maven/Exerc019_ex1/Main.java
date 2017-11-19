d9ecaf1c7988a2be386b6f280cf7c15ee9f23870
MadBar <madeleinetrollvik@gmail.com>
2017-11-19 12:20:26 +0100

    Chanfes for PC... Borde kunna sparas under en annan fork kasnke..?
    Men kan ju inte det än, så here goes!

--- src/main/java/com/example/maven/Exerc019_ex1/Main.java
+++ src/main/java/com/example/maven/Exerc019_ex1/Main.java
@@ -1,15 +1,16 @@
+/*package com.example.maven.Exerc019_ex1;*/
 package com.example.maven.Exerc019_ex1;
 
 public class Main {
 
 	public static void main(String[] args) {
-		MyRandomMethods mRM = new MyRandomMethods();
+		MyRandomMethods mrm = new MyRandomMethods();
 
-		System.out.println(mRM.randomBetween1And10());
-		System.out.println(mRM.randomBetween11And20());
-		System.out.println(mRM.randomBetween21And30());
-		System.out.println(mRM.randomBetween31And40());
-		System.out.println(mRM.randomBetween41And50());
+		System.out.println(mrm.randomBetween1And10());
+		System.out.println(mrm.randomBetween11And20());
+		System.out.println(mrm.randomBetween21And30());
+		System.out.println(mrm.randomBetween31And40());
+		System.out.println(mrm.randomBetween41And50());
 
 	}
 
